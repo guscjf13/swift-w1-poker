5a4a3ceb7149cefa1239b7c9b4fb696f4111c3cb
gatherheart <gatherheart@naver.com>
2021-01-06 17:43:24 +0900

    [ADD] Basic Card class

--- PokerApp/PokerApp/main.swift
+++ PokerApp/PokerApp/main.swift
@@ -7,5 +7,49 @@
 
 import Foundation
 
-print("Hello, World!")
+class Card : CustomStringConvertible {
+  
+    public enum Suit : String {
+        case Hearts = "♥️", Spades = "♠️", Diamonds = "💎", Clubs = "♣️"
+    }
+    public enum Rank : Int {
+        case A = 1, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, J, Q, K
+    }
+    enum ValidationError: Error {
+        case wrongRankRange
+    }
+    
+    var rank: Int
+    var suit: Suit
+    
+    public init(rank: Int, suit: Suit) throws {
+        guard 1...13 ~= rank else {
+            throw ValidationError.wrongRankRange
+        }
+        self.rank = rank
+        self.suit = suit
+    }
+    
+    private func convertRank() -> String {
+        switch self.rank {
+        case 1:
+            return "A"
+        case 11:
+            return "J"
+        case 12:
+            return "Q"
+        case 13:
+            return "K"
+        default:
+            return String(self.rank)
+        }
+    }
+    
+    var description: String {
+        return "\(self.suit.rawValue)\(self.convertRank())"
+    }
+}
 
+var c = try Card(rank: 10, suit: Card.Suit.Clubs)
+
+print(c)
